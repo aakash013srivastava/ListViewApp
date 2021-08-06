3aea10ddbbc6ab8a1767909bd5f780ff77898140
Aakash <aakash013srivastava@gmail.com>
2021-08-06 15:45:14 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/listviewapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.listviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.*;
+
+public class MainActivity extends AppCompatActivity {
+    EditText listItem;
+    Button addButton,viewList;
+    TextView displayListItem;
+    ArrayList<String> items = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listItem = findViewById(R.id.editTextItem);
+        addButton = findViewById(R.id.addListItemButton);
+        viewList = findViewById(R.id.viewList);
+        displayListItem = findViewById(R.id.displayListItem);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String itemDesc = listItem.getText().toString();
+                if(itemDesc.length()>0){
+                    items.add(itemDesc);
+                    String output = "";
+                    Iterator it = items.iterator();
+                    while(it.hasNext()){
+                        output += it.next()+"\n";
+                    }
+                    displayListItem.setText(output);
+                }
+            }
+        });
+
+
+    }
+
+}
